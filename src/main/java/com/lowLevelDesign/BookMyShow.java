af1fd782052ce30de68dcf71ef9663c84f210c46
Arpit505 <84476593+Arpit505@users.noreply.github.com>
2025-05-17 20:02:11 +0530

    test : made changes

---
+++ src/main/java/com/lowLevelDesign/BookMyShow.java
@@ -0,0 +1,27 @@
+//package main.java.com.lowLevelDesign;
+//
+//public class BookMyShow {
+//
+//    BookMyShow() {
+//        movieController = new MovieController();
+//        theatreController = new TheatreController();
+//    }
+//
+//    public static void main(String args[]) {
+//        BookMyShow bookMyShow = new BookMyShow();
+//        bookMyShow.initialize();
+//         //user1
+//        bookMyShow.createBooking(City.Blr, "RRR");
+//
+//        //user2
+//        bookMyShow.createBooking(City.mumbai, "RRR");
+//    }
+//
+//    private void initialize()
+//    {
+//        createMovies();
+//        createTheatre();
+//    }
+//
+//
+//}
---
+++ src/main/java/com/lowLevelDesign/DecoratorPattern.java
@@ -0,0 +1,93 @@
+package main.java.com.lowLevelDesign;
+
+interface Coffee {
+    double getCost();
+    String getDescription();
+}
+
+class BasicCoffee implements Coffee {
+    public double getCost() {
+        return 5.0;
+    }
+
+    public String getDescription() {
+        return "Plain Coffee";
+    }
+}
+
+abstract class CoffeeDecorator implements Coffee {
+    protected Coffee coffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    public double getCost() {
+        return coffee.getCost();
+    }
+
+    public String getDescription() {
+        return coffee.getDescription();
+    }
+}
+
+class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    public double getCost() {
+        return super.getCost() + 1.5;
+    }
+
+    public String getDescription() {
+        return super.getDescription() + ", Milk";
+    }
+}
+
+class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    public double getCost() {
+        return super.getCost() + 0.5;
+    }
+
+    public String getDescription() {
+        return super.getDescription() + ", Sugar";
+    }
+}
+
+class WhippedCreamDecorator extends CoffeeDecorator {
+    public WhippedCreamDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    public double getCost() {
+        return super.getCost() + 2.0;
+    }
+
+    public String getDescription() {
+        return super.getDescription() + ", Whipped Cream";
+    }
+}
+
+public class DecoratorPattern {
+    public static void main(String[] args) {
+        Coffee basicCoffee = new BasicCoffee();
+
+        // Decorate coffee with Milk
+        Coffee milkCoffee = new MilkDecorator(basicCoffee);
+
+        // Add Sugar
+        Coffee milkSugarCoffee = new SugarDecorator(milkCoffee);
+
+        // Add Whipped Cream
+        Coffee fancyCoffee = new WhippedCreamDecorator(milkSugarCoffee);
+
+        System.out.println("Description: " + fancyCoffee.getDescription());
+        System.out.println("Total Cost: $" + fancyCoffee.getCost());
+    }
+}
+
